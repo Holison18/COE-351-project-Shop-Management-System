d0e38daa96a075479dfb21698de3ae1726792eb7
HoLySon <kobbyholison2002@gmail.com>
2023-03-15 16:18:00 +0000

    create a file employee.txt

--- shop.cpp
+++ shop.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -22,6 +23,12 @@ class employee{
         password = pass;
         position = emp_pos;
         cout<<"Employee registered successfully"<<endl;
+
+        // write to file
+        fstream file;
+        file.open("employee.txt", ios::out | ios::app);
+        file<<first_name<<","<<last_name<<","<<id<<","<<password<<","<<position<<endl;
+        file.close();
     }
     void check_products(){
         // staff can check products
@@ -36,6 +43,9 @@ class employee{
 
 // declare an employee function
 void employee_funct();
+
+// declare customer function
+void customer();
 
 // main function
 int main(){
@@ -101,6 +111,31 @@ void employee_funct(){
     }
     else if(response==4){
         // sales
+    }
+    else{
+        cout<<"\n\t\t\t\t\tInvalid option"<<endl;
+    }
+}
+
+void customer(){
+    // customer can buy from the show 
+    // customer can view products to make a purchase 
+    // customer can search a product
+    int response;
+    cout<<"\t\t\t\tCustomer"<<endl;
+    cout<<"\t\t\t\t----------"<<endl;
+    cout<<"\t\t\t\t1. View product list"<<endl;
+    cout<<"\t\t\t\t2.Search product"<<endl;
+
+    //take response from user
+    cin>>response;
+    system("clear");
+
+    if(response==1){
+        // view product list
+    }
+    else if(response==2){
+        // search product
     }
     else{
         cout<<"\n\t\t\t\t\tInvalid option"<<endl;
